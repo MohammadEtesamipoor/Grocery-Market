f03f899448a61d3f80ff28ec9f65f78d5b0363b3
MohammadEtesamipoor <mohammad.etesamipoor78@gmail.com>
2025-08-17 20:18:03 +0330

    refactor: replace inputs with reusable Input component in Login and Register forms

--- components/common/auth/login/Register.tsx
+++ components/common/auth/login/Register.tsx
@@ -1,87 +1,82 @@
-// @flow
+
 import * as React from 'react';
-import {ImageComponent, Modal} from "@/components";
-import {ErrorMessage} from "@hookform/error-message";
-import {useForm} from "react-hook-form";
+import {ImageComponent, Input, Modal} from "@/components";
+import { useForm } from "react-hook-form";
+
+
 type Props = {
     setRegister: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export function Register({setRegister}: Props) {
-    const {register:inputRegister, handleSubmit,formState:{errors}}=useForm<FormData>();
+type FormData = {
+    email: string;
+    password: string;
+    "confirm-password": string;
+};
 
-    const onSubmit=(data:FormData)=>{
-        console.log(data)
-    }
+export function Register({ setRegister }: Props) {
+    const { register: inputRegister, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+    };
+
     return (
         <Modal>
-            <div className={"flex justify-between items-center  p-3 text-2xl text-NestMartTextHeading font-montserrat "}>
-                <div className={"flex gap-2 text-NestMartTextHeading"}>
-                    <h3 className={" cursor-pointer  p-2"}
-                        onClick={() => setRegister(prevState => !prevState)}>
-                        Login</h3>
+            <div className="flex justify-between items-center p-3 text-2xl text-NestMartTextHeading font-montserrat">
+                <div className="flex gap-2 text-NestMartTextHeading">
+                    <h3
+                        className="cursor-pointer p-2"
+                        onClick={() => setRegister(prevState => !prevState)}
+                    >
+                        Login
+                    </h3>
                     <div className="border-l-2 border-gray-400 h-12"></div>
-                    <h3 className={"cursor-pointer bg-gray-200 p-2 rounded-md"}>Register</h3>
+                    <h3 className="cursor-pointer bg-gray-200 p-2 rounded-md">Register</h3>
                 </div>
-                <p className={"cursor-pointer"} onClick={()=>setRegister( (prevState:boolean) => !prevState)}>Close</p>
+                <p className="cursor-pointer" onClick={() => setRegister(prevState => !prevState)}>Close</p>
             </div>
-            <hr className={"border-gray-300"} />
+            <hr className="border-gray-300" />
             <div className="flex min-h-full flex-col justify-center px-6 py-2 lg:px-8 bg-gray-100">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm flex justify-center flex-col items-center">
-                    <ImageComponent src={"/assets/images/login.png"} alt={"Company"}  width={100} height={100} />
+                    <ImageComponent src={"/assets/images/login.png"} alt={"Company"} width={100} height={100} />
                     <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-NestMartTextHeading">
                         Sign up create your account
                     </h2>
                 </div>
                 <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form onSubmit={handleSubmit(onSubmit)} action="#" method="POST" className="space-y-6">
-                        <div>
-                            <label htmlFor="email" className="text-gray-900">
-                                Email address</label>
-                            <div className="mt-1">
-                                <input
-                                    id="email"
-                                    type="email"
-                                    {...inputRegister("email",{required:"Enter email address"})}
-                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/normal"
-                                />
-                            </div>
-                            <ErrorMessage errors={errors} name="email"
-                                          render={({ message }) => <p className={"text-xs mt-1 text-red-600"}>{message}</p>}/>
-                        </div>
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">Password
-                                </label>
-                            </div>
-                            <div className="mt-2">
-                                <input
-                                    id="password"
-                                    type="password"
-                                    {...inputRegister("password",{required:true,minLength:{value:5,message:"Min 3 characters"}})}
-                                    className=" w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                                />
-                                <ErrorMessage errors={errors} name="password"
-                                              render={({ message }) => <p className={"text-xs mt-1 text-red-600"}>{message}</p>}/>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                                    Confirm password
-                                </label>
-                            </div>
-                            <div className="mt-2">
-                                <input
-                                    id="confirm-password"
-                                    type="confirm-password"
-                                    {...inputRegister("confirm-password",{required:true})}
-                                    className=" w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                                />
-                                <ErrorMessage errors={errors} name="confirm-password"
-                                              render={({ message }) => <p className={"text-xs mt-1 text-red-600"}>{message}</p>}/>
-                            </div>
-                        </div>
+
+                        <Input
+                            id="email"
+                            label="Email address"
+                            type="email"
+                            register={inputRegister}
+                            registerOptions={{ required: "Enter email address" }}
+                            errors={errors}
+                            placeholder="your.email@example.com"
+                        />
+
+                        <Input
+                            id="password"
+                            label="Password"
+                            type="password"
+                            register={inputRegister}
+                            registerOptions={{ required: true, minLength: { value: 5, message: "Min 3 characters" } }}
+                            errors={errors}
+                            placeholder="Enter password"
+                        />
+
+                        <Input
+                            id="confirm-password"
+                            label="Confirm password"
+                            type="password"
+                            register={inputRegister}
+                            registerOptions={{ required: "Please confirm your password" }}
+                            errors={errors}
+                            placeholder="Confirm your password"
+                        />
+
                         <button
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -89,11 +84,17 @@ export function Register({setRegister}: Props) {
                             Sign up
                         </button>
                     </form>
-                    <p className={"text-NestMartTextHeading mt-3"}>Already have an account? <span
-                        onClick={()=>setRegister(prevState => !prevState)}
-                        className={"text-red-300 cursor-pointer"}>Login </span></p>
+                    <p className="text-NestMartTextHeading mt-3">
+                        Already have an account?{" "}
+                        <span
+                            onClick={() => setRegister(prevState => !prevState)}
+                            className="text-red-300 cursor-pointer"
+                        >
+              Login
+            </span>
+                    </p>
                 </div>
             </div>
         </Modal>
     );
-};
+}
